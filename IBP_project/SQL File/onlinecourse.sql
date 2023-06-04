3e0e4f6efc0d0eacced43cc7eeb8d6ea109b9c07
betul <95382941+betulssahin@users.noreply.github.com>
2023-06-04 20:48:40 +0300

    Update onlinecourse.sql

--- IBP_project/SQL File/onlinecourse.sql
+++ IBP_project/SQL File/onlinecourse.sql
@@ -6,20 +6,6 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `onlinecourse`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `admin`
---
 
 CREATE TABLE `admin` (
                          `id` int(11) NOT NULL,
@@ -29,18 +15,12 @@ CREATE TABLE `admin` (
                          `updationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `admin`
---
 
 INSERT INTO `admin` (`id`, `username`, `password`, `creationDate`, `updationDate`) VALUES
     (1, 'admin', 'f925916e2754e5e03f75dd58a5733251', '2022-01-31 16:21:18', '2022-01-31 16:21:18');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `course`
---
+
 
 CREATE TABLE `course` (
                           `id` int(11) NOT NULL,
@@ -52,19 +32,13 @@ CREATE TABLE `course` (
                           `updationDate` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `course`
---
+
 
 INSERT INTO `course` (`id`, `courseCode`, `courseName`, `courseUnit`, `noofSeats`, `creationDate`, `updationDate`) VALUES
                                                                                                                        (1, 'PHP01', 'PHP', '5', 10, '2022-02-10 17:23:28', NULL),
                                                                                                                        (2, 'C001', 'C++', '12', 25, '2022-02-11 00:52:46', '11-02-2022 06:23:06 AM');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `courseenrolls`
---
 
 CREATE TABLE `courseenrolls` (
                                  `id` int(11) NOT NULL,
@@ -78,19 +52,12 @@ CREATE TABLE `courseenrolls` (
                                  `enrollDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `courseenrolls`
---
+
 
 INSERT INTO `courseenrolls` (`id`, `studentRegno`, `pincode`, `session`, `department`, `level`, `semester`, `course`, `enrollDate`) VALUES
                                                                                                                                         (1, '10806121', '822894', 1, 1, 2, 3, 1, '2022-02-11 00:59:33'),
                                                                                                                                         (2, '10806121', '822894', 1, 1, 1, 2, 2, '2022-02-11 01:01:07');
 
--- --------------------------------------------------------
-
---
--- Table structure for table `department`
---
 
 CREATE TABLE `department` (
                               `id` int(11) NOT NULL,
@@ -98,19 +65,13 @@ CREATE TABLE `department` (
                               `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `department`
---
+
 
 INSERT INTO `department` (`id`, `department`, `creationDate`) VALUES
                                                                   (1, 'IT', '2022-02-10 17:23:04'),
                                                                   (2, 'HR', '2022-02-10 17:23:09');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `level`
---
 
 CREATE TABLE `level` (
                          `id` int(11) NOT NULL,
@@ -118,20 +79,14 @@ CREATE TABLE `level` (
                          `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `level`
---
+
 
 INSERT INTO `level` (`id`, `level`, `creationDate`) VALUES
                                                         (1, '1', '2022-02-11 00:59:02'),
                                                         (2, '2', '2022-02-11 00:59:02'),
                                                         (3, '3', '2022-02-11 00:59:09');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `news`
---
 
 CREATE TABLE `news` (
                         `id` int(11) NOT NULL,
@@ -140,19 +95,13 @@ CREATE TABLE `news` (
                         `postingDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `news`
---
+
 
 INSERT INTO `news` (`id`, `newstitle`, `newsDescription`, `postingDate`) VALUES
                                                                              (2, 'Test News', 'This is for testing. This is for testing.This is for testing.This is for testing.This is for testing.This is for testing.This is for testing.This is for testing.This is for testing.This is for testing.', '2022-02-10 17:36:50'),
                                                                              (3, 'New Course Started C#', 'This is sample text for testing.', '2022-02-11 00:54:38');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `semester`
---
 
 CREATE TABLE `semester` (
                             `id` int(11) NOT NULL,
@@ -161,20 +110,14 @@ CREATE TABLE `semester` (
                             `updationDate` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `semester`
---
+
 
 INSERT INTO `semester` (`id`, `semester`, `creationDate`, `updationDate`) VALUES
                                                                               (1, '1', '2022-02-10 17:22:49', NULL),
                                                                               (2, '2', '2022-02-10 17:22:55', NULL),
                                                                               (3, '3', '2022-02-11 00:51:43', NULL);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `session`
---
 
 CREATE TABLE `session` (
                            `id` int(11) NOT NULL,
@@ -182,18 +125,12 @@ CREATE TABLE `session` (
                            `creationDate` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `session`
---
+
 
 INSERT INTO `session` (`id`, `session`, `creationDate`) VALUES
     (1, '2022', '2022-02-10 17:10:59');
 
--- --------------------------------------------------------
 
---
--- Table structure for table `students`
---
 
 CREATE TABLE `students` (
                             `StudentRegno` varchar(255) NOT NULL,
@@ -209,18 +146,11 @@ CREATE TABLE `students` (
                             `updationDate` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `students`
---
 
 INSERT INTO `students` (`StudentRegno`, `studentPhoto`, `password`, `studentName`, `pincode`, `session`, `department`, `semester`, `cgpa`, `creationdate`, `updationDate`) VALUES
-    ('10806121', '', 'f925916e2754e5e03f75dd58a5733251', 'Anuj kumar', '822894', NULL, NULL, NULL, '7.10', '2022-02-11 00:53:31', NULL);
+    ('10806121', '', 'f925916e2754e5e03f75dd58a5733251', 'John Doe', '822894', NULL, NULL, NULL, '7.10', '2022-02-11 00:53:31', NULL);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `userlog`
---
 
 CREATE TABLE `userlog` (
                            `id` int(11) NOT NULL,
@@ -231,138 +161,86 @@ CREATE TABLE `userlog` (
                            `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `userlog`
---
+
 
 INSERT INTO `userlog` (`id`, `studentRegno`, `userip`, `loginTime`, `logout`, `status`) VALUES
                                                                                             (1, '10806121', 0x3a3a3100000000000000000000000000, '2022-02-11 00:55:07', NULL, 1),
                                                                                             (2, '10806121', 0x3a3a3100000000000000000000000000, '2022-02-11 00:57:00', NULL, 1),
                                                                                             (3, '10806121', 0x3a3a3100000000000000000000000000, '2022-02-11 00:57:22', '11-02-2022 06:31:26 AM', 1);
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `admin`
---
 ALTER TABLE `admin`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `course`
---
+
 ALTER TABLE `course`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `courseenrolls`
---
+
 ALTER TABLE `courseenrolls`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `department`
---
+
 ALTER TABLE `department`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `level`
---
+
 ALTER TABLE `level`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `news`
---
+
 ALTER TABLE `news`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `semester`
---
+
 ALTER TABLE `semester`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `session`
---
+
 ALTER TABLE `session`
     ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `students`
---
+
 ALTER TABLE `students`
     ADD PRIMARY KEY (`StudentRegno`);
 
---
--- Indexes for table `userlog`
---
+
 ALTER TABLE `userlog`
     ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `admin`
---
 ALTER TABLE `admin`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `course`
---
+
 ALTER TABLE `course`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `courseenrolls`
---
+
 ALTER TABLE `courseenrolls`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `department`
---
+
 ALTER TABLE `department`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `level`
---
+
 ALTER TABLE `level`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `news`
---
+
 ALTER TABLE `news`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
---
--- AUTO_INCREMENT for table `semester`
---
+
 ALTER TABLE `semester`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `session`
---
+
 ALTER TABLE `session`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `userlog`
---
+
 ALTER TABLE `userlog`
     MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
