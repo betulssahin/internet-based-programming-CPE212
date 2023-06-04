1f13e574b09b60cd17705be2c6aec75f73f0d8d9
betul <95382941+betulssahin@users.noreply.github.com>
2023-06-04 20:41:36 +0300

    Update config.php

--- IBP_project/admin/includes/config.php
+++ IBP_project/admin/includes/config.php
@@ -1,8 +1,8 @@
 <?php
-const DB_SERVER = 'localhost';
-const DB_USER = 'root';
-const DB_PASS = '';
-const DB_NAME = 'onlinecourse';
+define DB_SERVER = 'localhost';
+define DB_USER = 'root';
+define DB_PASS = '';
+define DB_NAME = 'onlinecourse';
 $con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 
 if (mysqli_connect_errno())
